338bb49eb786664c0442f784fbc4a9ccb1282b09
Tanisha <tjain054@uottawa.ca>
2019-11-10 16:45:55 -0700

    resolved few errors and added a date picker in register page

--- app/src/main/java/com/example/ishaaq/cliniqueue/RegisterPage.java
+++ app/src/main/java/com/example/ishaaq/cliniqueue/RegisterPage.java
@@ -1,13 +1,49 @@
 package com.example.ishaaq.cliniqueue;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class RegisterPage extends AppCompatActivity {
+
+    private EditText mDisplayDate;
+    private DatePickerDialog.OnDateSetListener  mDateSetlistener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        mDisplayDate = (EditText) findViewById(R.id.datepicker);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year  = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(RegisterPage.this,
+                        android.R.style.Theme_Holo_Light,
+                        mDateSetlistener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+
+        mDateSetlistener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+            }
+        };
     }
 }
