8845353593c2b83f4b1a1eeadc548943aa2d713a
maverickjames <57418267+maverickjames@users.noreply.github.com>
2019-11-10 13:17:51 -0500

    Jim's changes

---
+++ app/src/main/java/com/example/ishaaq/cliniqueue/Contact_clinic.java
@@ -0,0 +1,13 @@
+package com.example.ishaaq.cliniqueue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contact_clinic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_clinic);
+    }
+}
