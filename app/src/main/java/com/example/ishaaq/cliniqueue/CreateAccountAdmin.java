9093b1d643948a3b0365a84951dc9b7644eae657
Bebo594 <aabde128@uottawa.ca>
2019-11-17 16:25:00 -0500

    I added a sign-up window for the clinics Also, I added a filter clinic page

---
+++ app/src/main/java/com/example/ishaaq/cliniqueue/CreateAccountAdmin.java
@@ -0,0 +1,13 @@
+package com.example.ishaaq.cliniqueue;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateAccountAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_admin);
+    }
+}
