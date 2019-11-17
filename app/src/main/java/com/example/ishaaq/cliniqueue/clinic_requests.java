a0f59812fd15ec6d6aa57e429c2f62739ceafdd1
NourDowedar <nour.dowedar@gmail.com>
2019-11-17 16:37:53 -0500

    I added a new screen for clinic requests and edited the clinic list and notification screens Nour

---
+++ app/src/main/java/com/example/ishaaq/cliniqueue/clinic_requests.java
@@ -0,0 +1,13 @@
+package com.example.ishaaq.cliniqueue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clinic_requests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_requests);
+    }
+}
