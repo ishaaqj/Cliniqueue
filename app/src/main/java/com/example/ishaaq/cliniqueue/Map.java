d0b3cd1fb465e0a85ef5500f95f55748db3a0caa
Tanisha <tjain054@uottawa.ca>
2019-11-26 14:56:25 -0700

    improvised the maps

--- app/src/main/java/com/example/ishaaq/cliniqueue/Map.java
+++ app/src/main/java/com/example/ishaaq/cliniqueue/Map.java
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Map extends FragmentActivity implements OnMapReadyCallback {
@@ -42,10 +43,89 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        float zoomLevel = 12.0f;
+
         // Add a marker in Sydney and move the camera
         LatLng uOttawa = new LatLng(45.421946, -75.680325);
-        mMap.addMarker(new MarkerOptions().position(uOttawa).title("University of Ottawa Health Services"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(uOttawa));
+        Marker u = mMap.addMarker(new MarkerOptions().position(uOttawa).title("University of Ottawa Health Services"));
+        u.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uOttawa,zoomLevel));
+
+        LatLng rideau = new LatLng(45.431005, -75.681432);
+        Marker r = mMap.addMarker(new MarkerOptions().position(rideau).title("Rideau-Friel Medical Centre"));
+        r.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rideau,zoomLevel));
+
+        LatLng appleTreeSparks = new LatLng(45.420402, -75.702761);
+        Marker as1 = mMap.addMarker(new MarkerOptions().position(appleTreeSparks).title("Apple Tree Medical group Sparks Street"));
+        as1.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(appleTreeSparks,zoomLevel));
+
+        LatLng appleTreeBank = new LatLng(45.379068, -75.668762);
+        Marker b = mMap.addMarker(new MarkerOptions().position(appleTreeBank).title("Apple Tree Medical group Bank Street"));
+        b.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(appleTreeBank,zoomLevel));
+
+        LatLng appleTreeSlater = new LatLng(45.417463, -75.704249);
+        Marker as2 = mMap.addMarker(new MarkerOptions().position(appleTreeSlater).title("Apple Tree Medical group Slater Street"));
+        as2.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(appleTreeSlater,zoomLevel));
+
+        LatLng carleton = new LatLng(45.385589, -75.692576);
+        Marker c = mMap.addMarker(new MarkerOptions().position(carleton).title("Carleton University Medical Services"));
+        c.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(carleton,zoomLevel));
+
+//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//            @Override
+//            public boolean onMarkerClick(Marker marker) {
+//                int position = (int)(marker.getTag());
+//                Intent intent1= new Intent(Map.this,ClinicsListActivity.class);
+//                startActivity(intent1);
+//                return false;
+//            }
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker arg0) {
+                if(arg0 != null && arg0.getTitle().equals("University of Ottawa Health Services")){
+                    String string4 =  "University of Ottawa Health Services";
+                    Intent intent1 = new Intent(Map.this, ContactClinicActivity.class);
+                    intent1.putExtra("ClinicName",string4);
+                    startActivity(intent1);}
+
+                if(arg0 != null && arg0.getTitle().equals("Rideau-Friel Medical Centre")){
+                    String string3 =  "Rideau-Friel Medical Centre";
+                    Intent intent2 = new Intent(Map.this, ContactClinicActivity.class);
+                    intent2.putExtra("ClinicName",string3);
+                    startActivity(intent2);}
+
+                if(arg0 != null && arg0.getTitle().equals("Apple Tree Medical group Sparks Street")){
+                    String string2 =  "Apple Tree Medical group Sparks Street";
+                    Intent intent3 = new Intent(Map.this, ContactClinicActivity.class);
+                    intent3.putExtra("ClinicName",string2);
+                    startActivity(intent3);}
+
+                if(arg0 != null && arg0.getTitle().equals("Apple Tree Medical group Bank Street")){
+                    String string1 =  "Apple Tree Medical group Bank Street";
+                    Intent intent4 = new Intent(Map.this, ContactClinicActivity.class);
+                    intent4.putExtra("ClinicName",string1);
+                    startActivity(intent4);}
+
+                if(arg0 != null && arg0.getTitle().equals("Apple Tree Medical group Slater Street")){
+                    String string5 =  "Apple Tree Medical group Slater Street";
+                    Intent intent5 = new Intent(Map.this, ContactClinicActivity.class);
+                    intent5.putExtra("ClinicName",string5);
+                    startActivity(intent5);}
+
+                if(arg0 != null && arg0.getTitle().equals("Carleton University Medical Services")){
+                    String string6 =  "Carleton University Medical Services";
+                    Intent intent6 = new Intent(Map.this, ContactClinicActivity.class);
+                    intent6.putExtra("ClinicName",string6);
+                    startActivity(intent6);}
+
+            }
+        });
+
     }
 
     public void showAppointmentsButton(View view) {
